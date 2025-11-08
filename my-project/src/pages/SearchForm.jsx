daa10a5964725062632629bfdc1d4f353a6d0b40
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-11-08 10:25:25 +0530

    Update SearchForm.jsx

--- my-project/src/pages/SearchForm.jsx
+++ my-project/src/pages/SearchForm.jsx
@@ -26,7 +26,7 @@ const SearchForm = () => {
           type="text"
           name="search"
           placeholder="Search images (e.g. dog)"
-          className="w-full sm:flex-1 px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full sm:flex-1 px-4 py-2 border border-gray-300 text-black dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <button
           type="submit"
